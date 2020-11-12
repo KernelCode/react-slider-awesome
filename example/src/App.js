b56128e0dd32567215e33546ba498c03e807bd89
Abdullah Altahery <abdullahaltahery@gmail.com>
2020-11-12 21:58:29 +0300

    adding className injection

--- example/src/App.js
+++ example/src/App.js
@@ -3,72 +3,73 @@ import React from 'react'
 import { Slider } from 'react-slider-awesome'
 
 import 'react-slider-awesome/dist/index.css'
+import './App.css'
 
 const App = () => {
   /* for Right to left set  body{  direction: rtl; }  then rtl={true|false} */
   return (
     <Slider rtl={false} noEffects={false}>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/1.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/2.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/3.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/4.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/5.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/6.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/1.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/2.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/3.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/4.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/5.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/6.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/1.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/2.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/3.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/4.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/5.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/6.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/1.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/2.jpg'} />
       </div>
-      <div>
+      <div className={'my_class'}>
         <img src={'./imgs/3.jpg'} />
       </div>
     </Slider>
--- src/componets/item/index.js
+++ src/componets/item/index.js
@@ -2,16 +2,19 @@ import React from 'react'
 
 import style_rtl from '../style.rtl.css'
 import style_lrt from '../style.css'
+
 const Item = (props) => {
   let style = props.rtl ? style_rtl : style_lrt
   let box = style.box
   if (props.noEffects) {
     box = style.boxNoEffects
   }
-  return (
-    <div className={box} onMouseEnter={(e) => {}} onMouseLeave={(e) => {}}>
-      {props.children}
-    </div>
-  )
+
+  return React.Children.map(props.children, (child, index) => {
+    return React.cloneElement(child, {
+      key: index,
+      className: child.props.className ? box + ' ' + child.props.className : box
+    })
+  })
 }
 export default Item
