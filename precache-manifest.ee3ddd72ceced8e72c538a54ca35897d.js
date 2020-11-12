fb8c5f0487a7f22a8ebeb963e5b9677137999164
Abdullah Altahery <abdullahaltahery@gmail.com>
2020-11-12 17:03:19 +0300

    Updates

---
+++ precache-manifest.ee3ddd72ceced8e72c538a54ca35897d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde967a1719bedcecc7101cfc0c5511f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a34bd57da74eee0ffa5",
+    "url": "./static/css/main.c908d56f.chunk.css"
+  },
+  {
+    "revision": "95c68d045d727a9654e9",
+    "url": "./static/js/2.c3cd6f42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c3cd6f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a34bd57da74eee0ffa5",
+    "url": "./static/js/main.281a0f1d.chunk.js"
+  },
+  {
+    "revision": "193b2e4002f32fc7cd9f",
+    "url": "./static/js/runtime-main.ea0be085.js"
+  }
+]);
